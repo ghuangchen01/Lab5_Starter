2664500c134869398a29332250f5f3d0f1c1245d
010102030404 <haoaa2003@gmail.com>
2024-05-07 23:46:55 -0700

    readme update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Lab 5 - Starter
 Guan Huang Chen
+[Expose](assets/scripts/expose.js)
+[Explore](assets/scripts/explore.js)
+1. No, The cons of unit testing is that they can't test how individual componenets interact with each other.
+2. Yes, Because thats something can be tested on with testing unit, local small features.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,37 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+//email
+test ('valid email 1',()=>{
+  expect(isEmail('ddd@ucsd.edu')).toBe(true);
+})
+test ('valid email 2',()=>{
+  expect(isEmail('aaa@ucsd.edu')).toBe(true);
+})
+
+test ('invalid email 1',()=>{
+  expect(isEmail('ddd@ucsdedu')).toBe(false);
+})
+test ('invalid email 2',()=>{
+  expect(isEmail('aaa@ucsd..edu')).toBe(false);
+})
+
+//phone
+test ('valid phone 1',()=>{
+  expect(isPhoneNumber('626-555-5555')).toBe(true);
+})
+test ('valid phone 2',()=>{
+  expect(isPhoneNumber('575-555-5555')).toBe(true);
+})
+
+test ('invalid phone 1',()=>{
+  expect(isPhoneNumber('5559892325555')).toBe(false);
+})
+test ('invalid phone 2',()=>{
+  expect(isPhoneNumber('6265555555')).toBe(false);
+})
+
+//Strong password
+test ('valid strong password 1',()=>{
+  expect(isStrongPassword('G123321')).toBe(true);
+})
